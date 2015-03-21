336b05c4857064da908a596a8deee4b50187eeb2
Alister Fenix <a.fenix@icloud.com>
2015-03-20 19:04:32 -0700

    Add show post spec

--- spec/features/add_blog_posts_spec.rb
+++ spec/features/add_blog_posts_spec.rb
@@ -10,5 +10,19 @@ describe "the add a blog process" do
     click_on "Create Post"
     expect(posts_path).to have_content 'posts'
   end
-  
+
+  it "gives an error message when there is no title entered" do
+    visit new_post_path
+    click_on "Create Post"
+    expect(page).to have_content 'problem'
+  end
+
+  it "gives a success message when each element in the form is filled out" do
+    visit new_post_path
+    fill_in "Title", :with => "First Blog Post"
+    fill_in "Author", :with => "Virginia Woolf"
+    fill_in "Body", :with => "Language is wine upon the lips"
+    click_on "Create Post"
+    expect(page).to have_content 'successfully'
+  end
 end
