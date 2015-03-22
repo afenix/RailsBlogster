556875f6030e088fababccd558b48c0443c0d424
Alister Fenix <a.fenix@icloud.com>
2015-03-22 10:20:34 -0700

    Add comments with intitial controller, model and specs

--- spec/features/destroy_post_spec.rb
+++ spec/features/destroy_post_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe "destroy a blog post" do 
   it "annihilates a blog post" do 
     test_post = Post.create(title: "First Blog", body: "Happiness is blogging.", author: "Unknown")
-    visit posts_path(test_post)
-    click_button "Delete"
-    expect(page).to change(Post, :count).by(-1)
+    visit root_path
+    click_on "First Blog"
+    click_on 'Delete'
+    expect(page).to have_content "annihilated"
   end
 end
